d4b4f867e4d6c982885a3028393a98040137a685
Tulio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-03-09 12:05:52 -0300

    Revert "Update"
    This reverts commit d758a0751db2b3c4069d2ae10a575b0bdd5d1b13.

--- frontend/src/js/index/main.js
+++ frontend/src/js/index/main.js
@@ -4,23 +4,19 @@ const usernameInput = document.getElementById('username_i');
 const emailInput = document.getElementById('email_i');
 const pwdInput = document.getElementById('pwd_i');
 
-createAccBtn.addEventListener('click', async () => {
+createAccBtn.addEventListener('click', () => {
     try {
-        const res = fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
-            mehtod: "POST",
+        fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
+            mehtod: POST,
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
+            body: {
                 username: usernameInput.value,
                 email: emailInput.value,
                 password: pwdInput.value
-            })
+            }
         });
-        const data = await res.json();
-        if (!res.ok) {
-            
-        };
     } catch(error) {
         console.log(`Erro no frontend: ${error}`);
     } finally {
